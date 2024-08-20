d7cf9f2bd6882d3353c7c6f68652e7641b5d835e
Juan Bustamante <jpbustamanteb@correo.usbcali.edu.co>
2024-08-20 12:55:23 -0500

    August 12 2024

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function saveToLocalStorage() {
+
+}
+
+function loadFromLocalStorage() {
+
+}
+
+function allowDrop(ev) {
+    
+}
+
+function drag(ev) {
+    
+}
+
+function drop(ev) {
+    
+}
+
+function getLocation() {
+    
+}
